8d47dd8a502a210b9989cb99104606108308f7de
Kushagra Sharma <skushagra2003@gmail.com>
2021-10-14 01:43:37 +0530

    created react app

---
+++ webster-frontend/src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function HomePage() {
+    return (
+        <div>
+            <Navbar />
+            {/** Login Form */}
+            {/** Registration Form */}
+            {/** Footer */}
+        </div>
+    )
+}
+
+export default HomePage
