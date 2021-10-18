8dd73216fa1902a70c8e4193b15efbd7c509718c
Pranjul Mishra <pranjulmishra1283@gmail.com>
2021-10-18 19:41:43 +0530

    home page and login done

--- webster-frontend/src/components/HomePage.js
+++ webster-frontend/src/components/HomePage.js
@@ -1,27 +1,47 @@
 import React from 'react';
-import IconButton  from '@mui/material/IconButton';
 import { Link } from 'react-router-dom';
-
 import Navbar from './Navbar.js';
+import Footer from './Footer.js';
 import './HomePage.css'
 
 function HomePage() {
     return (
         <div>
-            {/* <Navbar /> */}
-            <Link to = '/login'>
-                <IconButton>
-                    <p>Login</p>
-                </IconButton>
+            <Navbar />
+
+            <div
+                className="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+                <div className="content text-center">
+                    <h1 className="text-white">A Place Where You Find Your own Shadow.</h1>
+                    <div className="fp justify-content-center align-items-center">
+                        <div>
+            <Link to='/login'>
+                <button className=" btn btn-secondary w-100 d-flex justify-content-center align-items-center">
+                    Login
+                </button>
             </Link>
-            <Link to = '/register'>
-                <IconButton>
-                    <p>Signup</p>
-                </IconButton>
+            </div>
+
+            <br></br>
+            <br></br>
+
+            <div className ="Dennis">
+            
+            <Link to='/register'>
+                <button className="btn btn-secondary w-100 d-flex justify-content-center align-items-center">
+                    Signup
+                </button>
             </Link>
-            {/* Footer */}
-        </div>
+            </div>
+            </div>
+                </div>
+            </div>
+            
+        <Footer/>
+        
+
+    </div>
     )
 }
 
-export default HomePage
+export default HomePage;
