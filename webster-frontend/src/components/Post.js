fc1b1771b7c05e010bd441dd3a26bb35297c70c7
unknown <salonisrivastava835@gmail.com>
2021-12-17 15:15:34 +0530

    feed

--- webster-frontend/src/components/Post.js
+++ webster-frontend/src/components/Post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar";
-function Post() {
+function Post({ username , caption , imageUrl}) {
 
     
     return (
@@ -15,14 +15,14 @@ function Post() {
 
 
                 />
-                <h3>Username</h3>
+                <h3>{ username }</h3>
 
                
 
             
                 </div>
             <img className='post_image' src='https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1140180560?k=20&m=1140180560&s=612x612&w=0&h=X_400OQDFQGqccORnKt2PHYvTZ3dBLeEnCH_hRiUQrY='></img>
-            <div className='post_text'><h4><strong>Username: </strong>Caption</h4></div>
+            <div className='post_text'><h4><strong>{ username }: </strong>{ caption }</h4></div>
         </div>
     )
 }
---
+++ webster-frontend/src/components/Test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './test.css'
+
+function Test() {
+    return (
+        
+            <video autoPlay muted loop id='bg'>
+                 <source src="test.mp4" type='video/mp4'></source>
+            </video>
+        
+    )
+}
+
+export default Test
