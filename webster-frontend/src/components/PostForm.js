b1acf1948401099554b49796bd23c2a388bf841e
unknown <salonisrivastava835@gmail.com>
2021-12-17 17:15:52 +0530

    frooti

--- webster-frontend/src/components/PostForm.js
+++ webster-frontend/src/components/PostForm.js
@@ -2,27 +2,32 @@ import React from 'react'
 
 function PostForm() {
     return (
-        <div className='form'>
-            <div className='content'>
+        <div>
+            <div class="form" style={{width: "400px"}}>
+                <form action="#">
                 <div class="row">
-                    <div class="col-25">
-                        <label for="profile">Upload Profile Image</label>
+                            <div class="col-25">
+                                <label for="profile">Upload Profile Image</label>
+                            </div>
+                            <div class="col-75">
+                            <input  type="file" id="img" name="img" accept="image/*"></input>
+                            
+                            </div>
+                        </div>
+                    
+                    
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="subject">Caption</label>
+                        </div>
+                        <div class="col-75">
+                            <textarea id="subject" name="subject" placeholder="Write something.." style={{height: "200px"}, {width: "200px"}}></textarea>
+                        </div>
                     </div>
-                    <div class="col-75">
-                        <input type="file" id="img" name="img" accept="image/*"></input>
-
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-25">
-                        <label for="bio">Bio</label>
-                    </div>
-                    <div class="col-75">
-                        <textarea id="bio" name="bio" placeholder="Tell us something about yourself..." style={{ height: "100px" }} ></textarea>
-                    </div>
-                </div>
-            </div></div>
+                    <button  type="submit" className="signup">Post!</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
